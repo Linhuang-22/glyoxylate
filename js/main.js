800a01dea8beacdd9b8b2460d166a27a9a878c33
Linhuang-22 <linhuang_22@163.com>
2015-08-30 00:29:25 +0800

    功能改进
    1、发送邮件索要（首页和产品页）和留言（首页）增加submit按钮。
    2、完成分享功能。
    3、新闻内容页增加前页与后页。
    需要改动：
    1、css文件有增加
    2、main.js文件置换
    3、增加2个model在html中
    4、增加二维码图片

--- js/main.js
+++ js/main.js
@@ -1,22 +1,49 @@
-function shareTSina(title,rLink,site,pic) {  
-		var top = window.screen.height / 2 - 250;  
-		var left = window.screen.width / 2 - 300;
-    // title = "标题。";  
-  // pic = $(".p-img img").attr("src");  
-   // rLink = "http://www.abc.com/heeh.html";  
-   pic = "http://www.jb51.net/images/logo.gif"
-   // pic2 = "http://assets.changyan.sohu.com/upload/asset/scs/images/pic/pic42_null.gif"
+//微博分享函数
+function shareTSina(title,rLink,picUrl) {  
+	var top = window.screen.height / 2 - 250;  
+    var left = window.screen.width / 2 - 300;
+    var pic = "http://123.57.24.32" + ( picUrl || "/GLF/resources/images/GL-LOGO.jpg" );
      
-    window.open("http://service.weibo.com/share/share.php?pic=" +encodeURIComponent(pic) +"&title=" +   
+    window.open("http://service.weibo.com/share/share.php?appkey=" + encodeURIComponent("2693511263") + "&ralateUid" + encodeURIComponent('5665190364') + "&pic=" +encodeURIComponent(pic) +"&title=" +   
     encodeURIComponent(title.replace(/&nbsp;/g, " ").replace(/<br \/>/g, " "))+ "&url=" + encodeURIComponent(rLink),  
     "分享至新浪微博",  
-    "height=500,width=600,top=" + top + ",left=" + left + ",toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no, status=no");  
+    "height=500,width=600,top=" + top + ",left=" + left + ",toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no, status=no");    
 } 
-$("footer .icon-weibo").click(function(e) {
-	shareTSina("欢迎访问乙醛酸分公司网站","http://123.57.24.32:8080/GLF/gl/index","3842512498",$("img").attr('src'))
-});
-$(".request .icon-weibo").click(function(e) {
-	shareTSina("欢迎索要技术资料","http://123.57.24.32:8080/GLF/gl/index","3842512498",$("img").attr('src'))
+$(function () { 
+    //底部微信的分享
+    if ($('footer .icon-wechat').parent('a').attr('data-toggle') != "modal") {
+        $('footer .icon-wechat').parent('a').attr({
+             'data-content': "<img src='images/weichat-qocde.jpg'>",
+             'data-original-title': '<h5>微信二维码</h5>',
+             'data-placement':'top',
+              'data-container': 'body',
+             'data-toggle': 'popover',
+             'href': 'JavaScript:',
+             'data-trigger':'focus'
+        }).popover({html : true });
+    };
+    //底部微博链接添加
+    $('footer .icon-weibo').parent('a').attr({
+        "href": 'http://www.weibo.com/GUOLINOZONE',
+        "target": '_blank'
+    });
+    //询问资料处微信分享代码
+    if ($('.request .icon-wechat').parent('a').attr('data-toggle') != "modal") {
+        $('.request .icon-wechat').parent('a').attr({
+             'data-content': "<img src='images/weichat-qocde.jpg'>",
+             'data-original-title': '<h5>微信二维码</h5>',
+             'data-placement':'bottom',
+             'data-container': 'body',
+             'data-toggle': 'popover',
+             'href': 'JavaScript:',
+             'data-trigger':'focus'
+        }).popover({html : true });
+    };
+    //询问资料处微博链接添加
+    $('.request .icon-weibo').parent('a').attr({
+        "href": 'http://www.weibo.com/GUOLINOZONE',
+        "target": '_blank'
+    });
 });
 
 !function() {
@@ -31,9 +58,17 @@ $(".request .icon-weibo").click(function(e) {
     var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
     // document.writeln("您的浏览设备为：");
     if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
-         $('#qrcodeCanvas').parent('li').attr({
+         $('.request .icon-wechat').parent('a').attr({//询问资料微信
              'data-toggle': 'modal',
-             'data-target': '#myModal'
+             'data-target': '#guanzhuweixin'
+         });
+         $('footer .icon-wechat').parent('a').attr({//底部微信
+             'data-toggle': 'modal',
+             'data-target': '#guanzhuweixin'
+         });
+         $('.news-content .icon-wechat').parents('li').attr({//新闻内容、案例内容页右侧微信分享
+             'data-toggle': 'modal',
+             'data-target': '#weixinModal'
          });
     } else {
     	if (navigator.userAgent.indexOf('Trident') > -1) {
@@ -55,9 +90,25 @@ $(".request .icon-weibo").click(function(e) {
 }()	
 // browserRedirect();
 
-$("footer .icon-wechat").click(function(e) {
-    if ($(this).parents("li").attr('data-toggle') != "modal") {
-        $("#qrcodeCanvas").fadeToggle(400);
-    };
+// $("footer .icon-wechat").click(function(e) {
+//     if ($(this).parent("a").attr('data-toggle') != "modal") {
+//         $("#qrcodeCanvas").fadeToggle(400);
+//     };
 
-}); 
+// }); 
+$(".news-content .icon-wechat").click(function(e) {//页面左侧分享
+    if ($(this).parents("li").attr('data-toggle') != "modal") {
+        $("#qrcodeCanvas").css({
+                'left': $(this).offset().left -75,
+                'top': $(this).offset().top+44,
+                'height':"170px"
+            }).fadeToggle(400);
+    };
+    e.preventDefault()
+});
+$(".news-content .icon-weibo").click(function(e) {
+    var title = $(".news-content h3:first").text() + "\n" + "\t" + $(".editor p:first").text();
+    var href = window.location.href;
+    var img = $(".editor img:first").attr('src');
+    shareTSina(title,href,img);
+});
